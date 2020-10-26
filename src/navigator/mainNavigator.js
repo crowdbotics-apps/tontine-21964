996e610aff0d6bb8d1ad6a82f1cb37421d67d127
Crowdbotics <team@crowdbotics.com>
2020-10-26 08:58:18 +0000

    remove package blueprint-react-native-blank-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -32,7 +32,6 @@ import CopyOfBlankScreen3161620Navigator from '../features/CopyOfBlankScreen3161
 import CopyOfBlankScreen3161619Navigator from '../features/CopyOfBlankScreen3161619/navigator';
 import CopyOfBlankScreen3161618Navigator from '../features/CopyOfBlankScreen3161618/navigator';
 import BlankScreen4161617Navigator from '../features/BlankScreen4161617/navigator';
-import BlankScreen3161616Navigator from '../features/BlankScreen3161616/navigator';
 import SignIn11161615Navigator from '../features/SignIn11161615/navigator';
 import NotificationList2161614Navigator from '../features/NotificationList2161614/navigator';
 import UserProfile3161613Navigator from '../features/UserProfile3161613/navigator';
@@ -71,7 +70,6 @@ CopyOfBlankScreen3161620: { screen: CopyOfBlankScreen3161620Navigator },
 CopyOfBlankScreen3161619: { screen: CopyOfBlankScreen3161619Navigator },
 CopyOfBlankScreen3161618: { screen: CopyOfBlankScreen3161618Navigator },
 BlankScreen4161617: { screen: BlankScreen4161617Navigator },
-BlankScreen3161616: { screen: BlankScreen3161616Navigator },
 SignIn11161615: { screen: SignIn11161615Navigator },
 NotificationList2161614: { screen: NotificationList2161614Navigator },
 UserProfile3161613: { screen: UserProfile3161613Navigator },
